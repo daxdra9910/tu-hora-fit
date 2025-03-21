4be9a2d3951b73ed678a902811c59951428a6586
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-03-21 00:26:39 -0500

    feat: se agrega contenido de register

--- src/app/modules/auth/pages/register/register.page.ts
+++ src/app/modules/auth/pages/register/register.page.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonImg, IonItem, IonList, IonText, IonInput } from '@ionic/angular/standalone';
+import { matchPasswordsValidator } from '../../utils/match-password-validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImg, IonList, IonItem, IonText, IonButton, IonInput, CommonModule, ReactiveFormsModule]
 })
 export class RegisterPage implements OnInit {
+  form!: FormGroup
 
-  constructor() { }
+  constructor(private readonly formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.setupForm();
   }
 
+  setupForm(): void {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+      birthdate: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validators: matchPasswordsValidator('password', 'confirmPassword')
+    })
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log('Registro: ', this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
