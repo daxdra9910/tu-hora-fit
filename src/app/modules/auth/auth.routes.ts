de6fa1e4e69dd11bcdc4a8c1594c76f98757ede8
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-03-20 21:57:42 -0500

    feat: se agrega la pagina olvide mi contraseña

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from "@angular/router";
 
 const routes: Routes = [
-    {
-        path: 'login',
-        loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage)
-    },
-    {
-        path: 'register',
-        loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
-    }
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then(m => m.RegisterPage)
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () => import('./pages/forgot-password/forgot-password.page').then((m) => m.ForgotPasswordPage)
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () => import('./pages/forgot-password/forgot-password.page').then(m => m.ForgotPasswordPage)
+  }
 ]
 
 export default routes;
