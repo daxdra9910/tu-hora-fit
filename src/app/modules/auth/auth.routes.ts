d4c8d203f8ef093fb63bb999e4ba6ffc99481d96
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-03-21 00:27:05 -0500

    feat: se incluye el layout en las rutas

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -2,21 +2,23 @@ import { Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage)
+    path: '',
+    loadComponent: () => import('./layouts/background-layout/background-layout.component').then((m) => m.BackgroundLayoutComponent),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./pages/register/register.page').then(m => m.RegisterPage)
+      },
+      {
+        path: 'forgot-password',
+        loadComponent: () => import('./pages/forgot-password/forgot-password.page').then((m) => m.ForgotPasswordPage)
+      },
+    ]
   },
-  {
-    path: 'register',
-    loadComponent: () => import('./pages/register/register.page').then(m => m.RegisterPage)
-  },
-  {
-    path: 'forgot-password',
-    loadComponent: () => import('./pages/forgot-password/forgot-password.page').then((m) => m.ForgotPasswordPage)
-  },
-  {
-    path: 'forgot-password',
-    loadComponent: () => import('./pages/forgot-password/forgot-password.page').then(m => m.ForgotPasswordPage)
-  }
 ]
 
 export default routes;
