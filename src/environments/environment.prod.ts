4b02c34b609ceaa49e689ba6fc7412730ab9818a
Daxdra <dahian.sanchez@correounivalle.edu.co>
2025-04-02 20:28:18 -0500

    core: agregar propiedades de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBypIWdaSu84VbZmVTEJ25rDmMhnJz1wwY",
+    authDomain: "tu-hora-fit.firebaseapp.com",
+    projectId: "tu-hora-fit",
+    storageBucket: "tu-hora-fit.firebasestorage.app",
+    messagingSenderId: "774141056560",
+    appId: "1:774141056560:web:3d94601a3f1be42f3bfff5"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBypIWdaSu84VbZmVTEJ25rDmMhnJz1wwY",
+    authDomain: "tu-hora-fit.firebaseapp.com",
+    projectId: "tu-hora-fit",
+    storageBucket: "tu-hora-fit.firebasestorage.app",
+    messagingSenderId: "774141056560",
+    appId: "1:774141056560:web:3d94601a3f1be42f3bfff5"
+  },
 };
 
 /*
