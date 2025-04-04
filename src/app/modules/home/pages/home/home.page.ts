acf9d0fb606906fdd61f61f6d9ebf314eed12f2c
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-04-03 22:49:00 -0500

    feat: add HomePage component with routing, template, styles, and basic tests

---
+++ src/app/modules/home/pages/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class HomePage implements OnInit {
+  authService = inject(AuthService);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  logOut() {
+    this.authService.logout();
+  }
+
+}
