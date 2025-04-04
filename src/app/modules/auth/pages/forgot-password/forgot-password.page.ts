fa28bfe4b6c1ef522a98bf2f29c0a52d7d2b39f9
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-04-03 22:47:50 -0500

    feat: update forgot password page with improved form handling and UI feedback

--- src/app/modules/auth/pages/forgot-password/forgot-password.page.ts
+++ src/app/modules/auth/pages/forgot-password/forgot-password.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonItem, IonInput, IonButton, IonImg, NavController } from '@ionic/angular/standalone';
+import { AuthService } from '../../services/auth.service';
+import { UtilsService } from 'src/app/modules/core/services/utils.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -21,25 +23,44 @@ import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonLi
     IonItem,
     IonInput,
     IonButton,
-    IonImg // 📌 IMPORTANTE: Se agrega IonImg para evitar el error NG8001
+    IonImg
   ]
 })
 export class ForgotPasswordPage implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  private readonly navCtrl = inject(NavController);
+  private readonly authService = inject(AuthService);
+  private readonly utilsService = inject(UtilsService);
+  
   form: FormGroup;
 
-  constructor(private fb: FormBuilder, private navCtrl: NavController) {
+  ngOnInit() {
     this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]] // 📌 Se define la variable 'email' dentro del formulario
+      email: ['', [Validators.required, Validators.email]]
     });
   }
 
-  ngOnInit() {}
-
-  enviarRecuperacion() {
+  async enviarRecuperacion() {
     if (this.form.valid) {
-      const email = this.form.value.email;
-      console.log("Enviando enlace de recuperación a:", email);
-      // Aquí puedes integrar el servicio para enviar el correo de recuperación
+      const loading = await this.utilsService.loading();
+      await loading.present();
+      this.authService.forgotPassword(this.form.value.email).then(() => {
+        this.utilsService.presentToast({
+          message: 'Se ha enviado a su correo electrónico el enlace para restablecer la contraseña',
+          duration: 2500,
+          animated: true,
+          color: 'primary',
+          position: 'top',
+          icon: 'alert-circle-outline'
+        }).catch((error) => this.utilsService.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'danger',
+          position: 'top',
+          icon: 'alert-circle-outline'
+        })
+        ).finally(() => loading.dismiss());
+      })
     } else {
       console.log("Formulario inválido");
     }
