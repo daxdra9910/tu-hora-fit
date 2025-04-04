506683cc7cf9521da2a05d1493ee127aaf31c583
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-04-03 22:47:42 -0500

    feat: update routing and guards for authentication flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
+import { antiAuthGuard, authGuard } from './modules/core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
+    canActivate: [antiAuthGuard],
     loadChildren: () => import('./modules/auth/auth.routes').then((m) => m.default),
+  },
+  {
+    path: '',
+    loadComponent: () => import('./modules/core/layouts/tab/tab.component').then(m => m.TabComponent),
+    children: [
+      {
+        path: 'home',
+        canActivate: [authGuard],
+        loadChildren: () => import('./modules/home/home.routes').then( m => m.default)
+      },
+    ]
   },
   {
     path: '',
