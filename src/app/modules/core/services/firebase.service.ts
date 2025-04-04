ab427c807fd6062c816b31888f7f331ab4892b8f
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-04-03 22:48:46 -0500

    feat: refactor FirebaseService to use inject for dependency management

--- src/app/modules/core/services/firebase.service.ts
+++ src/app/modules/core/services/firebase.service.ts
@@ -1,11 +1,11 @@
-import { Inject, Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { doc, Firestore, setDoc } from '@angular/fire/firestore'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
-  private readonly firestore = Inject(Firestore);
+  private readonly firestore = inject(Firestore);
 
   setDocument(path: string, data: any) {
     return setDoc(doc(this.firestore, path), data);
