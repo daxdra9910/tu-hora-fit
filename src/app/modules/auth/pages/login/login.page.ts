1df950146f6d546a69104d125bef6980046308c3
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-04-03 22:47:58 -0500

    feat: update login page UI and refactor constructor to use inject for services

--- src/app/modules/auth/pages/login/login.page.ts
+++ src/app/modules/auth/pages/login/login.page.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NavigationStart, Router, RouterModule } from '@angular/router';
-import { IonButton, IonContent, IonInput, IonItem, IonList, IonModal, IonImg, IonText } from '@ionic/angular/standalone';
+import { IonButton, IonContent, IonInput, IonItem, IonList, IonModal, IonImg, IonText, NavController } from '@ionic/angular/standalone';
 import { filter, Subscription } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { UtilsService } from 'src/app/modules/core/services/utils.service';
@@ -16,17 +16,16 @@ import { UtilsService } from 'src/app/modules/core/services/utils.service';
   imports: [IonInput, IonContent, IonList, IonItem, IonText, IonModal, IonButton, IonImg, CommonModule, ReactiveFormsModule, RouterModule]
 })
 export class LoginPage implements OnInit, OnDestroy {
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
+  private readonly utilsService = inject(UtilsService);
+  private readonly navCtrl = inject(NavController)
+
   @ViewChild('modal', { static: false }) modal!: IonModal;
 
   form!: FormGroup;
   subscriptions = new Subscription();
-
-  constructor(
-    private readonly formBuilder: FormBuilder,
-    private readonly router: Router,
-    private readonly authService: AuthService,
-    private readonly utilsService: UtilsService
-  ) {}
 
   ngOnInit(): void {
     this.setupForm();
@@ -61,11 +60,11 @@ export class LoginPage implements OnInit, OnDestroy {
       await loading.present();
       
       this.authService.singIn(this.form.value['email'], this.form.value['password'])
-      .then(user => console.log(user))
+      .then(() => this.navCtrl.navigateForward('/home'))
       .catch(error => this.utilsService.presentToast({
         message: error.message,
         duration: 2500,
-        color: 'green',
+        color: 'danger',
         position: 'middle',
         icon: 'alert-circle-outline'
       }))
