ed2daa55e389998b94dc629d1b00e2f9a3f4f3a5
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-04-08 22:38:49 -0500

    refactor: update login page layout and improve code formatting

--- src/app/modules/auth/pages/login/login.page.ts
+++ src/app/modules/auth/pages/login/login.page.ts
@@ -1,11 +1,21 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NavigationStart, Router, RouterModule } from '@angular/router';
-import { IonButton, IonContent, IonInput, IonItem, IonList, IonModal, IonImg, IonText, NavController } from '@ionic/angular/standalone';
-import { filter, Subscription } from 'rxjs';
-import { AuthService } from '../../services/auth.service';
-import { UtilsService } from 'src/app/modules/core/services/utils.service';
+import {CommonModule} from '@angular/common';
+import {Component, inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NavigationStart, Router, RouterModule} from '@angular/router';
+import {
+  IonButton,
+  IonContent,
+  IonInput,
+  IonItem,
+  IonList,
+  IonModal,
+  IonImg,
+  IonText,
+  NavController, IonInputPasswordToggle
+} from '@ionic/angular/standalone';
+import {filter, Subscription} from 'rxjs';
+import {AuthService} from '../../services/auth.service';
+import {UtilsService} from 'src/app/modules/shared/services/utils.service';
 
 
 @Component({
@@ -22,7 +32,7 @@ export class LoginPage implements OnInit, OnDestroy {
   private readonly utilsService = inject(UtilsService);
   private readonly navCtrl = inject(NavController)
 
-  @ViewChild('modal', { static: false }) modal!: IonModal;
+  @ViewChild('modal', {static: false}) modal!: IonModal;
 
   form!: FormGroup;
   subscriptions = new Subscription();
@@ -45,12 +55,12 @@ export class LoginPage implements OnInit, OnDestroy {
 
   subscribeRouterEvents(): void {
     const routerSub = this.router.events
-    .pipe(filter(event => event instanceof NavigationStart))
-    .subscribe(() => {
-      if (this.modal) {
-        this.modal.dismiss();
-      }
-    });
+      .pipe(filter(event => event instanceof NavigationStart))
+      .subscribe(() => {
+        if (this.modal) {
+          this.modal.dismiss();
+        }
+      });
     this.subscriptions.add(routerSub);
   }
 
@@ -58,17 +68,17 @@ export class LoginPage implements OnInit, OnDestroy {
     if (this.form.valid) {
       const loading = await this.utilsService.loading();
       await loading.present();
-      
+
       this.authService.singIn(this.form.value['email'], this.form.value['password'])
-      .then(() => this.navCtrl.navigateForward('/home'))
-      .catch(error => this.utilsService.presentToast({
-        message: error.message,
-        duration: 2500,
-        color: 'danger',
-        position: 'middle',
-        icon: 'alert-circle-outline'
-      }))
-      .finally(() => loading.dismiss());  
+        .then(() => this.navCtrl.navigateForward('/home'))
+        .catch(error => this.utilsService.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'danger',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        }))
+        .finally(() => loading.dismiss());
     } else {
       this.form.markAllAsTouched();
     }
