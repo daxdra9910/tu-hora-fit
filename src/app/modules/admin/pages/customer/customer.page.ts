7279c01fbdcd14ac993bdbcfb9b7709d468245bc
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-04-09 23:18:34 -0500

    feat: enhance customer page with search functionality and user state display

--- src/app/modules/admin/pages/customer/customer.page.ts
+++ src/app/modules/admin/pages/customer/customer.page.ts
@@ -1,40 +1,57 @@
 import {Component, inject, OnInit} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 import {
-  IonContent,
-  IonHeader,
-  IonTitle,
-  IonToolbar,
-  IonSearchbar,
-  IonGrid,
-  IonCol,
-  IonRow,
+  IonAvatar,
   IonButton,
-  IonIcon, IonList, IonItemSliding, IonItem, IonLabel, IonAvatar, IonImg, IonItemOptions, IonItemOption
+  IonChip,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonList,
+  IonRow,
+  IonSearchbar,
+  IonText,
+  IonTitle,
+  IonToolbar
 } from '@ionic/angular/standalone';
 import {UserService} from "../../../core/services/user.service";
 import {UserModel} from "../../../shared/models/user.model";
+import {StatePipe} from "../../../shared/pipes/state.pipe";
+import {StateEnum} from "../../../shared/enums/state.enum";
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.page.html',
   styleUrls: ['./customer.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonGrid, IonCol, IonRow, IonButton, CommonModule, FormsModule, IonIcon, IonList, IonItemSliding, IonItem, IonLabel, IonAvatar, IonImg, IonItemOptions, IonItemOption]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonButton, CommonModule, FormsModule, IonIcon, IonList, IonItemSliding, IonItem, IonAvatar, IonImg, IonItemOptions, IonItemOption, IonText, IonGrid, IonRow, IonCol, IonChip, StatePipe]
 })
 export class CustomerPage implements OnInit {
   private readonly userService = inject(UserService);
+
   users: UserModel[];
 
+  activeState = StateEnum.ACTIVE;
+
   ngOnInit() {
-    this.findUsers();
+    this.filterUsers();
   }
 
-  findUsers() {
-    this.userService.getAllUsers().then(users => this.users = users);
+  handleInput(event: Event) {
+    const target = event.target as HTMLIonSearchbarElement;
+    const query = target.value;
+    this.filterUsers(query);
   }
 
-  searchTerm: string = '';
-  constructor() { }
+  filterUsers(search: string = '') {
+    this.userService.searchUsers(search).then(users => this.users = users);
+  }
 }
