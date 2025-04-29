a57f502458ff7b51e9c0edb199455514d198c45a
Dahian Alexandra Sanchez <dahiansanchez505@gmail.com>
2025-04-29 17:44:53 -0500

    feat(inventory): agregar ruta /admin/inventories al módulo de rutas de administración

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -1,14 +1,18 @@
-import {Routes} from "@angular/router";
+import { Routes } from "@angular/router";
 
 const routes: Routes = [
   {
     path: 'customers',
     loadComponent: () => import('./pages/customer/customer.page').then((m) => m.CustomerPage),
-  },  {
+  },
+  {
     path: 'plans',
-    loadComponent: () => import('./pages/plans/plans.page').then( m => m.PlansPage)
+    loadComponent: () => import('./pages/plans/plans.page').then(m => m.PlansPage)
+  },
+  {
+    path: 'inventario',
+    loadComponent: () => import('./pages/inventory/inventory.page').then(m => m.InventoryPage)
   }
-
-]
+];
 
 export default routes;
