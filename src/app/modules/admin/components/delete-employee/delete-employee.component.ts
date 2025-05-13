f53ec61779fa3921809f547f4863391a216c24fb
Dahian Alexandra Sanchez <dahiansanchez505@gmail.com>
2025-05-12 23:31:35 -0500

    feat(class): crear componente para eliminar clase

---
+++ src/app/modules/admin/components/delete-employee/delete-employee.component.ts
@@ -0,0 +1,77 @@
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonAlert } from '@ionic/angular/standalone';
+
+import { UtilsService } from '../../../shared/services/utils.service';
+import { EmployeeModel } from '../../../shared/models/employed.model';
+import { EmployeeService } from '../../../core/services/employee.service';
+
+@Component({
+  selector: 'app-delete-employee',
+  standalone: true,
+  imports: [
+    IonAlert,
+    CommonModule
+  ],
+  templateUrl: './delete-employee.component.html',
+  styleUrls: ['./delete-employee.component.scss']
+})
+export class DeleteEmployeeComponent {
+  private readonly employeeService = inject(EmployeeService);
+  private readonly utilsService = inject(UtilsService);
+
+  @Input() isOpen = false;
+  @Output() isOpenChange = new EventEmitter<void>();
+
+  @Input() employeeData: EmployeeModel | null = null;
+
+  alertButtons = [
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+      handler: () => {}
+    },
+    {
+      text: 'Aceptar',
+      role: 'confirm',
+      handler: async () => await this.confirmDelete()
+    }
+  ];
+
+  toggleOpen(): void {
+    this.isOpenChange.emit();
+  }
+
+  async confirmDelete(): Promise<void> {
+    if (!this.employeeData) return;
+
+    const loading = await this.utilsService.loading();
+    await loading.present();
+
+    this.employeeService.deleteEmployee(this.employeeData)
+      .then(async () => {
+        await this.utilsService.presentToast({
+          message: 'Empleado eliminado correctamente',
+          duration: 2500,
+          position: 'bottom',
+          color: 'success',
+          icon: 'trash-bin'
+        });
+        this.toggleOpen();
+      })
+      .catch(async (error) => {
+        await this.utilsService.presentToast({
+          message: error.message || 'Error al eliminar empleado',
+          duration: 2500,
+          position: 'bottom',
+          color: 'danger',
+          icon: 'alert-circle-outline'
+        });
+      })
+      .finally(() => loading.dismiss());
+  }
+
+  get message(): string {
+    return `¿Está seguro de que desea eliminar al empleado ${this.employeeData?.name}?`;
+  }
+}
