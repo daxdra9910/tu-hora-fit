b47ea60f82d35c2f85dae714f523d5ad5e9c90c1
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-06-24 21:45:27 -0500

    feat: refactor SchedulePage to inject ScheduleService and load schedules on initialization

--- src/app/modules/admin/pages/schedule/schedule.page.ts
+++ src/app/modules/admin/pages/schedule/schedule.page.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonText, IonModal } from '@ionic/angular/standalone';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { CreateScheduleComponent } from "../../components/create-schedule/create-schedule.component";
+import { ScheduleService } from 'src/app/modules/core/services/schedule.service';
+import { ScheduleClassModelWithId } from 'src/app/modules/shared/models/schedule-class.model';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.page.html',
   styleUrls: ['./schedule.page.scss'],
   standalone: true,
-  imports: [IonModal, IonText, IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, CreateScheduleComponent]
+  imports: [IonText, IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, CreateScheduleComponent]
 })
 export class SchedulePage implements OnInit {
+  private readonly scheduleService = inject(ScheduleService);
+
+  schedules: ScheduleClassModelWithId[] = [];
 
   isCreating = false;
 
   ngOnInit(): void {
+    this.loadSchedules();
+  }
+
+  loadSchedules(): void {
+    this.scheduleService.getAllSchedules()
+    .then(schedules => {
+      this.schedules = schedules;
+    })
   }
 
   openCreate(): void {
