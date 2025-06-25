f79b81e9aadd00ad0974ac6aa8228edf1991653f
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-06-24 21:45:20 -0500

    feat: update create-schedule component to handle end recurrence and integrate schedule creation logic

--- src/app/modules/admin/components/create-schedule/create-schedule.component.ts
+++ src/app/modules/admin/components/create-schedule/create-schedule.component.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IonModal, IonToolbar, IonContent, IonHeader, IonTitle, IonButtons, IonButton, IonIcon, IonItem, IonInput, IonLabel, IonAvatar, IonList, IonSelect, IonSelectOption } from "@ionic/angular/standalone";
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonList, IonModal, IonSelect, IonSelectOption, IonTitle, IonToolbar } from "@ionic/angular/standalone";
 import { ClassService } from 'src/app/modules/core/services/class.service';
 import { EmployeeService } from 'src/app/modules/core/services/employee.service';
+import { ScheduleService } from 'src/app/modules/core/services/schedule.service';
 import { RecurrenceEnum } from 'src/app/modules/shared/enums/recurrence.enum';
 import { ClassModelWithIdAndImage } from 'src/app/modules/shared/models/class.model';
 import { EmployeeModelWithIdAndImage } from 'src/app/modules/shared/models/employed.model';
+import { ScheduleClassModel } from 'src/app/modules/shared/models/schedule-class.model';
 import { UtilsService } from 'src/app/modules/shared/services/utils.service';
 
 @Component({
   selector: 'app-create-schedule',
   standalone: true,
-  imports: [IonList, IonAvatar, IonLabel, IonInput, IonItem, IonIcon, IonButton, IonButtons, IonTitle, IonModal, IonToolbar, IonContent, IonHeader, IonSelect, IonSelectOption, ReactiveFormsModule, CommonModule],
+  imports: [IonList, IonInput, IonItem, IonIcon, IonButton, IonButtons, IonTitle, IonModal, IonToolbar, IonContent, IonHeader, IonSelect, IonSelectOption, ReactiveFormsModule, CommonModule],
   templateUrl: './create-schedule.component.html',
   styleUrls: ['./create-schedule.component.scss'],
 })
@@ -21,6 +23,7 @@ export class CreateScheduleComponent implements OnInit {
   private readonly utilsService = inject(UtilsService);
   private readonly classService = inject(ClassService);
   private readonly employeeService = inject(EmployeeService);
+  private readonly scheduleService = inject(ScheduleService);
 
   @Input() isOpen: boolean = false;
   @Output() isOpenChange = new EventEmitter<void>()
@@ -76,6 +79,32 @@ export class CreateScheduleComponent implements OnInit {
 
     const loading = await this.utilsService.loading();
     await loading.present();
+
+    const data: ScheduleClassModel = {
+      ...this.formGroup.value
+    }
+
+    this.scheduleService.createSchedule(data)
+    .then(() => {
+        this.utilsService.presentToast({
+          message: 'Horario creado correctamente',
+          duration: 2500,
+          position: 'bottom',
+          color: 'success',
+          icon: 'checkmark-circle'
+        });
+        this.toggleOpen();
+      })
+      .catch((error) => {
+        this.utilsService.presentToast({
+          message: error.message || 'Error al crear el horario',
+          duration: 2500,
+          position: 'bottom',
+          color: 'danger',
+          icon: 'alert-circle-outline'
+        });
+      })
+      .finally(() => loading.dismiss());
   }
 
 }
